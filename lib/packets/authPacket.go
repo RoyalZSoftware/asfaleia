2774aec487717ac0136102cc8c094ba1c6bc808e
IJustDev <alexanderpanovi@yahoo.de>
2020-12-26 01:34:25 +0100

    refactor: authpacket functions

--- lib/packets/authPacket.go
+++ lib/packets/authPacket.go
@@ -1,7 +1,6 @@
 package packets
 
 import (
-	"fmt"
 	"github.com/royalzsoftware/asfaleia/lib"
 	"github.com/royalzsoftware/asfaleia/lib/utils"
 	"math/rand"
@@ -12,6 +11,31 @@ func generateRandomValidationMessage(size int) []byte {
 	token := make([]byte, size)
 	rand.Read(token)
 	return token
+}
+
+func generateRandomEncodedValidationMessage(size int) string {
+	return utils.EncodeInBase64(generateRandomValidationMessage(size))
+}
+
+func generateFinishedValidationMessage(validationMessage string,
+	pubKey string) (error, []byte) {
+	encrypter := &utils.RSAEncrypter{}
+	err, publicKey := utils.BytesToPublicKey([]byte(pubKey))
+
+	if err != nil {
+		return err, []byte("")
+	}
+
+	err, encryptedMessage := encrypter.Encrypt(
+		[]byte(validationMessage),
+		publicKey,
+	)
+
+	if err != nil {
+		return err, []byte("")
+	}
+
+	return nil, encryptedMessage
 }
 
 /*
@@ -34,38 +58,25 @@ func (p AuthPacket) VerifyParameters(params []string) bool {
 }
 
 func (p AuthPacket) Handle(params []string, c net.Conn) {
-	wrongUsage := &WrongUsagePacket{}
-
-	encrypter := &utils.RSAEncrypter{}
-	err, publicKey := utils.BytesToPublicKey([]byte(params[0]))
-
-	if err != nil {
-		lib.SendAnswerPacket(wrongUsage, c)
-		return
-	}
-
-	validationMessage := utils.EncodeInBase64(
-		generateRandomValidationMessage(10),
+	validationMessage := generateRandomEncodedValidationMessage(10)
+	err, encryptedMessage := generateFinishedValidationMessage(
+		validationMessage,
+		params[0],
 	)
-	fmt.Println(string(validationMessage))
-	handle := len(lib.HandleQueue)
-	err, encryptedMessage := encrypter.Encrypt(
-		[]byte(validationMessage),
-		publicKey,
-	)
-
-	var answer lib.ResponsePacket
 	if err != nil {
-		answer = wrongUsage
+		lib.SendAnswerPacket(&WrongUsagePacket{}, c)
 	} else {
-		answer = &AuthVerifyIdentityPacket{
+		handle := len(lib.HandleQueue)
+		lib.HandleQueue[handle] = string(validationMessage)
+
+		answer := &AuthVerifyIdentityPacket{
 			Handle:           handle,
 			EncryptedMessage: utils.EncodeInBase64(encryptedMessage),
 		}
-		lib.HandleQueue[handle] = string(validationMessage)
+
+		lib.SendAnswerPacket(answer, c)
 	}
 
-	lib.SendAnswerPacket(answer, c)
 }
 
 func (p AuthPacket) Identifier() int {
