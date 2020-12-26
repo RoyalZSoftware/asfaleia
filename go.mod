07ef1d858757f0349d9827e58bc7a2b60651ea14
IJustDev <alexanderpanovi@yahoo.de>
2020-12-26 02:08:56 +0100

    feat: basic testing of authpacket, update go.mod

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/royalzsoftware/asfaleia
 
 go 1.13
+
+require github.com/matryer/is v1.4.0
---
+++ lib/packets/test/authPacket_test.go
@@ -0,0 +1,48 @@
+package test
+
+import (
+	"github.com/matryer/is"
+	"github.com/royalzsoftware/asfaleia/lib/packets"
+	"testing"
+)
+
+func TestAuthPacketParameterVerification(t *testing.T) {
+	is := is.New(t)
+	packet := &packets.AuthPacket{}
+
+	isValid := packet.VerifyParameters([]string{"Test"})
+
+	is.True(!isValid)
+}
+
+func TestAuthPacketReturnsWrongUsage(t *testing.T) {
+	is := is.New(t)
+	packet := &packets.AuthPacket{}
+
+	responsePacket := packet.TryCreateVerifyPacket(
+		"ValidationMessage",
+		"Test",
+		0,
+	)
+
+	is.Equal(responsePacket.Identifier(), 102)
+}
+
+func TestAuthPacketReturnsValidationMessage(t *testing.T) {
+	is := is.New(t)
+	packet := &packets.AuthPacket{}
+
+	publicKey := "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC1DGPUS" +
+		"PqPJdcWvdBNM5ymAQlKIXbLMa3X4D0JaBKKqv47w4ii3" +
+		"sLw2lckEHk+8LGyBNkbQN0aaVxWeuo4R7IsBMXVpJgvf" +
+		"/iQImRm5b9HIxkAYi5PqYGcHKBqiuzuHmpPlGYdGpM+h" +
+		"K5EeWiDj/sRVcP42KyYzGmd6ExbqaWMewIDAQAB"
+
+	responsePacket := packet.TryCreateVerifyPacket(
+		"ValidationMessage",
+		publicKey,
+		0,
+	)
+
+	is.Equal(responsePacket.Identifier(), 101)
+}
